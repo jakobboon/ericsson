3bda759f9b290657fa4d31401ec0b8d8ec211997
jakobboon <124922822+jakobboon@users.noreply.github.com>
2023-03-14 10:09:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+console.log("is this shit working")
+
+function playartificalmp3 (){
+    $('#artificalmp3')[0].play();
+}
+
+$(".artificalgraphic").click(playartificalmp3);
+
+function playautotechmp3 (){
+    $('#autotechmp3')[0].play();
+}
+
+$(".autotechgraphic").click(playautotechmp3);
+
+function playbachmp3 (){
+    $('#bachmp3')[0].play();
+}
+
+$(".bachgraphic").click(playbachmp3);
+
+function playbigbeatmp3 (){
+    $('#bigbeatmp3')[0].play();
+}
+
+$(".bigbeatgraphic").click(playbigbeatmp3);
+
+function playbouncemp3 (){
+    $('#bouncemp3')[0].play();
+}
+
+$(".bouncegraphic").click(playbouncemp3);
+
+function playclubalivemp3 (){
+    $('#clubalivemp3')[0].play();
+}
+
+$(".clubalivegraphic").click(playclubalivemp3);
+
+function playcrystalmp3 (){
+    $('#crystalmp3')[0].play();
+}
+
+$(".crystalgraphic").click(playcrystalmp3);
+
+function playdangermp3 (){
+    $('#dangermp3')[0].play();
+}
+
+$(".dangergraphic").click(playdangermp3);
+
+function playdestinymp3 (){
+    $('#destinymp3')[0].play();
+}
+
+$(".destinygraphic").click(playdestinymp3);
+
+function playdigitalismp3 (){
+    $('#digitalismp3')[0].play();
+}
+
+$(".digitalisgraphic").click(playdigitalismp3);
+
+function playdogsmp3 (){
+    $('#dogsmp3')[0].play();
+}
+
+$(".dogsgraphic").click(playdogsmp3);
+
+function playluciousmp3 (){
+    $('#luciousmp3')[0].play();
+}
+
+$(".luciousgraphic").click(playluciousmp3);
+
+function playmelancholicsmp3 (){
+    $('#melancholicsmp3')[0].play();
+}
+
+$(".melancholicsgraphic").click(playmelancholicsmp3);
+
+function playsunbeamzmp3 (){
+    $('#sunbeamzmp3')[0].play();
+}
+
+$(".sunbeamzgraphic").click(playsunbeamzmp3);
+
+function playnotlatemp3 (){
+    $('#notlatemp3')[0].play();
+}
+
+$(".notlategraphic").click(playnotlatemp3);
+
